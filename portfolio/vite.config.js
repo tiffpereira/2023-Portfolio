eb4dbb297b401c02c8b78a2b0655e3e8f6566a8c
Tiffany Pereira <tiffanympereira@gmail.com>
2023-09-22 09:24:16 -0400

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/portfolio',
+  base: '/2023-Portfolio/portfolio',
   plugins: [react()],
 })
